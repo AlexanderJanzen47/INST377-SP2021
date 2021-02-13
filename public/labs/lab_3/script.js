a498adb349f3bfe761a2fe9ef5814fb1c1d34d1c
Alexander Janzen <alexanderjanzen@hotmail.com>
2021-02-12 22:20:13 -0500

    Lab 3

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,1 +1,27 @@
 /* Put your javascript in here */
+
+const pngArray = [];
+for (let i = 0; i < carousel.querySelectorAll(".carousel div").length; i++) 
+{
+  pngArray.push(carousel.querySelectorAll('.carousel div')[i].dataset.background)
+};
+
+/* configuration */
+let width = 130;
+let count = 3;
+let list = carousel.querySelector('ul');
+let listElems = carousel.querySelectorAll('li');
+
+let position = 0;
+
+carousel.querySelector('.prev').onclick = function() {
+  position += width * count,
+  position = Math.min(position, 0),
+  list.style.marginLeft = position + 'px'
+};
+
+carousel.querySelector('.next').onclick = function() {
+  position -= width * count,
+  position = Math.max(position, -width * (listElems.length - count)),
+  list.style.marginLeft = position + 'px'
+};
